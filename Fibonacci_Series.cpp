67cc276cb388a0d4e3fc245455577c929cadad52
sriharithemass <103378692+sriharithemass@users.noreply.github.com>
2024-06-30 17:22:41 +0530

    Update Fibonacci_Series.cpp

--- Fibonacci_Series.cpp
+++ Fibonacci_Series.cpp
@@ -7,3 +7,24 @@ public:
         return fib(n-1)+fib(n-2);
     }
 };
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)
+        return 0;
+
+        int prev=1;
+        int curr=1;
+        int next;
+
+        for(int i=0;i<n-2;i++)
+        {
+            next=curr+prev;
+            prev=curr;
+            curr=next;
+        }
+
+        return curr;
+    }
+};
